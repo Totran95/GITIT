76be8ec3cb7fab41b48eab67c643cdb02eb4818b
Totran95 <totte.rantanen@gmail.com>
2025-10-26 12:49:35 +0200

    Initial commit

---
+++ Ikaohjelma/src/IkaOhjelma.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IkaOhjelma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Anna ikäsi: ");
+        int ika = scanner.nextInt();
+
+        if (ika >= 0 && ika < 18) {
+            System.out.println("Olet alaikäinen");
+        } else if (ika >= 65) {
+            System.out.println("Olet eläkeläinen");
+        } else {
+            System.out.println("Olet aikuinen");
+        }
+    }
+}
